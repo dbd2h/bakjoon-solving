48f97a2ef6ec1a761426ea14ada330d7f7b6c4b4
dbd2h <kangminchang1@gmail.com>
2026-07-18 17:21:05 +0900

    two pointers continue

--- 900-codeforce/GYMEDU/ITMO/TwoPointer/step3/B.cpp
+++ 900-codeforce/GYMEDU/ITMO/TwoPointer/step3/B.cpp
@@ -8,10 +8,44 @@
 #include <set>
 typedef long long ll;
 typedef long double lld;
+#define LEN 100001
 
 using namespace std;
 
 int main()
 {
-    
+    int n;
+    ll s;
+    cin>>n>>s;
+    ll arr[LEN];
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    int l=0;
+    int r=0;
+    ll sumV=0;
+    ll res=0;
+    ll len=0;
+    while(l<n)
+    {
+        if(sumV>s)
+        {
+            len-=(r-l);
+            res+=len;
+            l++;
+            sumV-=arr[l];
+        }
+        else
+        {
+            if(r==n)
+            {
+                res+=len;
+                len-=(r-l);
+                l++;
+                continue;
+            }
+            r++;
+            len+=(r-l);
+            sumV+=arr[r];
+        }
+    }
+    cout<<res;
 }
--- 900-codeforce/GYMEDU/ITMO/TwoPointer/step3/C.cpp
+++ 900-codeforce/GYMEDU/ITMO/TwoPointer/step3/C.cpp
@@ -8,10 +8,31 @@
 #include <set>
 typedef long long ll;
 typedef long double lld;
+#define LEN 300001
 
 using namespace std;
 
 int main()
 {
-    
+    int n,R;
+    cin>>n>>R;
+    int arr[LEN];
+    for(int i=1;i<=n;i++) cin>>arr[i];
+    int l=1;
+    int r=1;
+    ll res=0;
+    while(r<=n)
+    {
+        int dis=arr[r]-arr[l];
+        if(dis<=R)
+        {
+            r++;
+        }
+        else
+        {
+            res+=(n-r+1);
+            l++;
+        }
+    }
+    cout<<res;
 }
